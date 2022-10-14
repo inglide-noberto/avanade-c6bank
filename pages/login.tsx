cb4d2df443dc4fbaaddd454cb507d266f76d815a
Inglide Noberto <inglidenoberto961@gmail.com>
2022-10-14 15:53:00 -0300

    validação de senha com uso de UseEffect e useState, contribuição com snackbar

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useState } from "react";
+import React, { FormEvent, useEffect, useState } from "react";
 import {
     Typography,
     Container,
@@ -9,10 +9,20 @@ import {
     Grid,
     Box,
     CssBaseline,
-    FormControlLabel
+    FormControlLabel,
+    Stack,
+    Snackbar
 } from '@mui/material';
 import Link from "next/link";
 import { createTheme, ThemeProvider } from '@mui/material/styles'
+import MuiAlert, { AlertProps } from '@mui/material/Alert';
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+    props,
+    ref,
+) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 type CopyProps = {
     site?: string;
@@ -35,29 +45,63 @@ function Copyright(props: CopyProps) {
 const theme = createTheme();
 
 export default function LoginPage() {
+    const [email, setEmail] = useState<string | null | undefined>('');
     const [password, setPassword] = useState<string | null | undefined | FormDataEntryValue>('');
-    const [error, setError] = useState<string | boolean>('');
-    const [errorMessage, setErrorMessage] = useState<string>('');
+    const [error, setError] = useState<boolean>(false);
+    const [errorMensage, setErrorMensage] = useState<string>('');
+    const [open, setOpen] = useState<boolean>(false);
+    const [contador, setContador] = useState<number>(0);
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+
+    // executado automáticamente após o primeiro render da page
+    // executa a cada vez que recarrega a page
+    useEffect(() => {
+        if (contador == 0) {
+            document.title = `Executando userEffect a primeira vez. Contador: ${contador}`;
+        } else {
+            document.title = `Executou o userEffect ${contador} vezes.`;
+            // vai sp
+        }
+        console.log(`Executou o contador ${contador} vezes.`);
+    }, [contador]);
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const data = new FormData(e.currentTarget);
 
         setPassword(data.get('password'));
-        if(password && password.length < 6){
-            setError(true);
-            setErrorMessage("Tá de brinks né? Senha curta!!");
-        }
-        console.log({
-            email: data.get('email'),
-            password: data.get('password'),
-        });
+
     }
+
+    useEffect(() => {
+        if (password && password.length < 6) {
+            setError(true);
+            setErrorMensage('A senha dever ter no mínimo 6 caracteres');
+        } else if(password) {
+            setError(false);
+            setErrorMensage('');
+
+            // chamar a API do server para validar usuário e senha.
+            // adicionar o snackbar
+            setOpen(true);
+            // fazer o redirect
+        }
+    }, [password]);
 
     return (
         <ThemeProvider theme={theme}>
             <Container component="main" maxWidth="xs">
                 <CssBaseline />
+                <Stack>
+                    <Snackbar open={open} autoHideDuration={5000} onClose={handleClose}>
+                        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+                            Usuário logado com sucesso... Aguarde!
+                        </Alert>
+                    </Snackbar>
+                </Stack>
                 <Box sx={{
                     mt: 8,
                     display: 'flex',
@@ -78,7 +122,7 @@ export default function LoginPage() {
                         <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
                             Login
                         </Button>
-                        {error && <Typography color="error">{errorMessage}</Typography>}
+                        {error && <Typography color="error">{errorMensage}</Typography>}
                     </Box>
                 </Box>
                 <Copyright site="avanade" />
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  background-image: url('/barco.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
 
 export default function RegisterPage() {
   return (
-    <div>register</div>
+    <>
+      <Container>register</Container>
+    </>
   );
 };
